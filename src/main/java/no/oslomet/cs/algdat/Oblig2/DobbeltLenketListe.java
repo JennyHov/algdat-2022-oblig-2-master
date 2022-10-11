a960ba143294bef7cb407f279a2169110bcd42c2
Joachim IDM Martinsen <joachimivoidm@gmail.com>
2022-10-11 21:45:28 +0200

    lagt til kildekode oppg.3

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -63,7 +63,17 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     // Programkode 1.2.3 a) fra kompendiet
     private static void fratilKontroll(int antall, int fra, int til) {
-        
+        if (fra < 0)
+            throw new IndexOutOfBoundsException
+                    ("fra(" + fra + ") er negativ!");
+
+        if (til > antall)
+            throw new IndexOutOfBoundsException
+                    ("til(" + til + ") > antall(" + antall + ")");
+
+        if (fra > til)
+            throw new IllegalArgumentException
+                    ("fra(" + fra + ") > til(" + til + ") - illegalt intervall!");
     }
 
     public Liste<T> subliste(int fra, int til) {
